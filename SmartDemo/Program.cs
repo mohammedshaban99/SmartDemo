bbcd867b9bb08ff78b173391e7db8bfe493ad616
Mohamed Shaban <mohammedshaban1458@gmail.com>
2026-09-21 12:15:26 +0300

    Add project files.

---
+++ SmartDemo/Program.cs
@@ -0,0 +1,34 @@
+using SmartDemo.Services;
+using SmartDemo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDevExpressBlazor(options => {
+    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
+});
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<DxThemesService>();
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AllowAnonymous();
+
+app.Run();
