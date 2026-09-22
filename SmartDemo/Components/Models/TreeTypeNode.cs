f4edde4b2b1d6bf5480ef66588672f607344d0c4
Mohamed Shaban <mohammedshaban1458@gmail.com>
2026-09-22 15:24:24 +0300

    change

---
+++ SmartDemo/Components/Models/TreeTypeNode.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartDemo.Components.Models
+{
+    public class TreeTypeNode
+    {
+        public string Name { get; set; } = "";
+        public string Code { get; set; } = "";
+        public int Level { get; set; } = 0;
+        public bool IsExpanded { get; set; } = false;
+        public List<TreeTypeNode> Children { get; set; } = new();
+    }
+}
